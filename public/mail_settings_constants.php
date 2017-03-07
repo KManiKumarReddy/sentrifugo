3214f6a670ba5a487e87067f374b98314566ad44
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2017-03-08 01:40:21 +0530

    SnedgridCreds

---
+++ public/mail_settings_constants.php
@@ -0,0 +1,8 @@
+<?php
+defined('MAIL_SMTP') || define('MAIL_SMTP','');
+defined('MAIL_USERNAME') || define('MAIL_USERNAME','app64727517@heroku.com');
+defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','w76efoy93667');
+defined('MAIL_PORT') || define('MAIL_PORT','587');
+defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+defined('MAIL_TLS') || define('MAIL_TLS','TLS');
+?>
