d703783ab835a7696cf3a881ff2d9230ff126407
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2017-03-08 01:16:42 +0530

    Cleardbcreds

---
+++ public/db_constants.php
@@ -0,0 +1,6 @@
+<?php
+defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','us-cdbr-iron-east-03.cleardb.net');
+defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','b190373915615c');
+defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','914ccf28');
+defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','heroku_3e89daa0c0e1624');
+?>
